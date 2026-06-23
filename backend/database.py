8ad42c962822d33adbfaef85bdb45f251d9359fd
Farzan R S <farzanrichu610@gmail.com>
2026-06-23 20:18:05 +0530

    set variable for db name

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 import sqlite3
 
+
+DB_NAME = "device_statistics.db"
 def get_db_connection():
-    conn = sqlite3.connect("device_statistics.db")
+    conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
 
