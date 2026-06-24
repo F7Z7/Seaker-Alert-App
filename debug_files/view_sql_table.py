26d60baad6d2df339dd8d595159b7b33aff76edd
Farzan R S <farzanrichu610@gmail.com>
2026-06-24 15:04:35 +0530

    sql table prints per second find

--- debug_files/view_sql_table.py
+++ debug_files/view_sql_table.py
@@ -1,3 +1,5 @@
+import time
+
 from backend.database import get_db_connection
 
 conn = get_db_connection()
@@ -6,8 +8,17 @@ data = conn.execute('SELECT * FROM system_data').fetchall()
 
 print("hey")
 
-print(f"row length {len(data)}")
-for row in data:
-    print(dict(row))
+# print(f"row length {len(data)}")
+# for row in data:
+#     print(dict(row))
+rows_before = len(data)
+
+time.sleep(10)
+
+rows_after = len(conn.execute(
+    "SELECT * FROM system_data"
+).fetchall())
+
+print(rows_after - rows_before)
 
 conn.close()
