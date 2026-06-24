dfcacdcdb1683628a0b70279198c8bd7b8cd7f50
Farzan R S <farzanrichu610@gmail.com>
2026-06-24 15:24:23 +0530

    fixed url from local host to ip based

--- backend/static/js/dashboard.js
+++ backend/static/js/dashboard.js
@@ -12,29 +12,72 @@ window.addEventListener('load', () => {
 
     async function latestData() {
         try {
+            const response = await fetch('http://127.0.0.1:5000//api/latest');
 
-
-            const response = await fetch('http://localhost:5000/api/latest')
             if (!response.ok) {
                 console.log("HTTP Error:", response.status);
                 return;
             }
 
-            let data = await response.json()
-            document.getElementById("ti")
-        }
-        catch (error) {
+            const data = await response.json();
+
+            // Uptime
+            document.getElementById("uptime").innerText =
+                `${data.uptime_hours.toFixed(1)} h`;
+
+            // CPU
+            document.getElementById("cpu-pct").innerText =
+                data.cpu_usage.toFixed(1);
+
+            document.getElementById("cpu-bar").style.width =
+                `${data.cpu_usage}%`;
+
+            document.getElementById("cpu-cores").innerText =
+                data.cpu_cores;
+
+            document.getElementById("cpu-threads").innerText =
+                data.cpu_threads;
+
+            // Memory
+            document.getElementById("mem-pct").innerText =
+                data.percentage.toFixed(1);
+
+            document.getElementById("mem-bar").style.width =
+                `${data.percentage}%`;
+
+            document.getElementById("mem-total").innerText =
+                `${data.total_gb.toFixed(1)} GB`;
+
+            document.getElementById("mem-used").innerText =
+                `${data.used_gb.toFixed(1)} GB`;
+
+            document.getElementById("mem-avail").innerText =
+                `${data.available_gb.toFixed(1)} GB`;
+
+            // Disk
+            document.getElementById("disk-pct").innerText =
+                data.disk_percent.toFixed(1);
+
+            document.getElementById("disk-bar").style.width =
+                `${data.disk_percent}%`;
+
+            document.getElementById("disk-total").innerText =
+                `${data.disk_total_gb.toFixed(1)} GB`;
+
+            document.getElementById("disk-used").innerText =
+                `${data.disk_used_gb.toFixed(1)} GB`;
+
+            document.getElementById("disk-free").innerText =
+                `${data.disk_free_gb.toFixed(1)} GB`;
+
+        } catch (error) {
             console.error("Error fetching latest data:", error);
-            return 404;
         }
     }
 
     async function start() {
-        while(true) {
-            await latestData()
-            setInterval(latestData, 1000)
-        }
-
+        await latestData()
+        setInterval(latestData, 1000)
     }
 
     start()
