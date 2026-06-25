2913ed9440ed0d36cca2c863d46933a5559bedd5
Farzan R S <farzanrichu610@gmail.com>
2026-06-26 01:09:51 +0530

    tweeked the run settings

--- run.py
+++ run.py
@@ -7,5 +7,10 @@ if __name__ == "__main__":
     init_db()  # this is for first time creation of tables
     t=threading.Thread(target=collector,daemon=True)
     t.start()
-    app.run(debug=True, use_reloader=False)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+        use_reloader=False
+    )
 
