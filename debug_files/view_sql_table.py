a1bd57868dbaddabdfd017f8e5ff53c18f91c9e4
Farzan R S <farzanrichu610@gmail.com>
2026-06-25 17:47:34 +0530

    verified the threshold table

--- debug_files/view_sql_table.py
+++ debug_files/view_sql_table.py
@@ -4,13 +4,13 @@ from backend.database import get_db_connection
 
 conn = get_db_connection()
 
-data = conn.execute('SELECT * FROM system_data').fetchall()
+data = conn.execute('SELECT * FROM thresholds').fetchall()
 
 # print("hey")
 
 print(f"row length {len(data)}")
-# for row in data:
-#     print(dict(row))
+for row in data:
+    print(dict(row))
 # rows_before = len(data)
 
 # time.sleep(10)
