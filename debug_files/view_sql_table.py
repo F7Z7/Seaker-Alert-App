cc6526a39d26ebb320e2429f6521140aa2274286
Farzan R S <farzanrichu610@gmail.com>
2026-06-26 10:50:19 +0530

    debug

--- debug_files/view_sql_table.py
+++ debug_files/view_sql_table.py
@@ -6,17 +6,13 @@ conn = get_db_connection()
 cursor=conn.cursor()
 
 data = cursor.execute('''
-    SELECT timestamp,cpu_usage ,memory_percentage
-    FROM system_data
-    ORDER BY timestamp DESC
-    LIMIT 30
-    ''').fetchall()
+    SELECT * FROM thresholds
+    ''').fetchone()
 
 # print("hey")
 
 print(f"row length {len(data)}")
-for row in data:
-    print(dict(row))
+print(data["cpu"], data["memory"], data["disk"])
 # rows_before = len(data)
 
 # time.sleep(10)
