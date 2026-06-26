0dc83fa08fe60cdcb66d6382184d021d65316855
Farzan R S <farzanrichu610@gmail.com>
2026-06-26 15:14:37 +0530

    added a two minute cool down between alerts for avoiding spam

--- backend/alerts.py
+++ backend/alerts.py
@@ -1,45 +1,53 @@
+import time
 from backend.thresholds import load_threshold
-from backend.telegrarm_alert import  send_alert
+from backend.telegrarm_alert import send_alert
+
+COOLDOWN = 120  # seconds (2 minutes)
 
 last_alert = {
-    "cpu": False,
-    "memory": False,
-    "disk": False
+    "cpu": 0,
+    "memory": 0,
+    "disk": 0
 }
 
+
+def can_send_alert(component):
+    current_time = time.time()
+
+    if current_time - last_alert[component] >= COOLDOWN:
+        last_alert[component] = current_time
+        return True
+
+    return False
+
+
 def check_thresholds(data):
-    # print("ivde")
     cpu_t, mem_t, disk_t = load_threshold()
 
     cpu = data["cpu"]["usage_percent"]
     mem = data["memory"]["percentage"]
     disk = data["disk"]["percentage"]
-    # print("hey1")
-    if cpu > cpu_t:
-        # print("hey2")
-        if not last_alert["cpu"]:
-            # print("hey3")
-            send_alert(
-                f" CPU usage exceeded threshold\nCurrent: {cpu}%\nThreshold: {cpu_t}%"
-            )
-            last_alert["cpu"] = True
-    else:
-        last_alert["cpu"] = False
 
-    if mem > mem_t:
-        if not last_alert["memory"]:
-            send_alert(
-                f" Memory usage exceeded threshold\nCurrent: {mem}%\nThreshold: {mem_t}%"
-            )
-            last_alert["memory"] = True
-    else:
-        last_alert["memory"] = False
+    # CPU
+    if cpu > cpu_t and can_send_alert("cpu"):
+        send_alert(
+            f" CPU usage exceeded threshold\n"
+            f"Current: {cpu}%\n"
+            f"Threshold: {cpu_t}%"
+        )
 
-    if disk > disk_t:
-        if not last_alert["disk"]:
-            send_alert(
-                f" Disk usage exceeded threshold\nCurrent: {disk}%\nThreshold: {disk_t}%"
-            )
-            last_alert["disk"] = True
-    else:
-        last_alert["disk"] = False
+    # Memory
+    if mem > mem_t and can_send_alert("memory"):
+        send_alert(
+            f" Memory usage exceeded threshold\n"
+            f"Current: {mem}%\n"
+            f"Threshold: {mem_t}%"
+        )
+
+    # Disk
+    if disk > disk_t and can_send_alert("disk"):
+        send_alert(
+            f" Disk usage exceeded threshold\n"
+            f"Current: {disk}%\n"
+            f"Threshold: {disk_t}%"
+        )
